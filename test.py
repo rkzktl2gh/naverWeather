eb0ba7c3ccd7bd14ae901322dcb128599b96a710
sjkang <kootaehoon12@naver.com>
2019-05-21 20:15:44 +0900

    입출력함수 생성과 메뉴선택 기능 추가

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 from naverWeather import *
 while(1):
-    print("메뉴를 선택하세요.")
-    print("1번 : 검색, 2번 : 종료")
-    control = int(input())
-    if control == 1: # 1번 입력 시
-        print("지역명을 입력해주세요.")
-        area=input()
-        if area in naverWeather.map_cityNum:
+ print("메뉴를 선택하세요.")
+ print("1번 : 검색, 2번 : 종료")
+ control = int(input())
+ if control == 1: # 1번 입력 시
+  print("지역명을 입력해주세요.")
+  area=input()
+  if area in naverWeather.map_cityNum:
+   print(naverWeather(area).getWeather())
+  elif area =='': print(naverWeather("서울").getWeather())
+  else:# 잘못된 도시명 입력 시
+   print("잘못된 도시명입니다. 다시 입력하세요")
+   area=input()
+   if area in naverWeather.map_cityNum:
+    print(naverWeather(area).getWeather())
+   else:
+     while(area not in naverWeather.map_cityNum):
+         print("잘못된 도시명입니다. 다시입력하세요")
+         area=input()
+         if area in naverWeather.map_cityNum:
             print(naverWeather(area).getWeather())
-           elif area =='': print(naverWeather("서울").getWeather())
-           else:# 잘못된 도시명 입력 시
-               print("잘못된 도시명입니다. 다시 입력하세요")
-               area=input()
-               if area in naverWeather.map_cityNum:
-                   print(naverWeather(area).getWeather())
-               else:
-                   while(area not in naverWeather.map_cityNum):
-                       print("잘못된 도시명입니다. 다시입력하세요")
-                       area=input()
-                       if area in naverWeather.map_cityNum:
-                           print(naverWeather(area).getWeather())
 else:
     print("프로그램을 종료합니다.")
     break
