36ca342e089e1887605a364814ed7bf04fbe33ca
sanghyun <rkzktl2gh@naver.com>
2019-05-22 17:22:51 +0900

    초기에 입력받을 때 모든 경우에 대한 대처

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from naverWeather import *
 while(1):
  print("메뉴를 선택하세요.")
  print("1번 : 검색, 2번 : 종료")
- control = int(input())
- if control == 1: # 1번 입력 시
+ control = input()
+ if control == '1': # 1번 입력 시
   print("지역명을 입력해주세요.")
   area=input()
   if area in naverWeather.map_cityNum: # 올바른 도시명 입력 시
@@ -20,6 +20,8 @@ while(1):
           area=input()
           if area in naverWeather.map_cityNum:
               print(naverWeather(area).getWeather())
- else: # 2번 입력 시
+ elif control == '2': # 2번 입력 시
   print("프로그램을 종료합니다.")
   break
+ else:
+     print("다시입력해주세요")
