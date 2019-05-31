8f17cc20ed6ec29c585684360eba427065a0fd4f
rkzktl2gh <48742418+rkzktl2gh@users.noreply.github.com>
2019-05-31 20:04:25 +0900

    코드수정 - 1

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from naverWeather import *
+from room import *
+
 while(1):
  print("메뉴를 선택하세요.")
- print("1번 : 검색 | 2번 : 종료")
+ print("1번 : 검색 | 2번 : 옷장 | 3번 : 종료")
  control = input()
  if control == '1': # 1번 입력 시
   print("지역명을 입력해주세요.")
@@ -17,6 +19,24 @@ while(1):
        if area in naverWeather.map_cityNum:
            print(naverWeather(area).getWeather())
  elif control == '2': # 2번 입력 시
+  while(1):
+      print("-------------------------------------------------")
+      print("                <<< 옷     장 >>>                ")
+      print("1번 : 상의 | 2번 : 하의 | 3번 : 신발 | 4번 : 종료")
+      print("-------------------------------------------------")
+      roomnum = input()
+      if roomnum == '1':
+          room.room_1()
+      elif roomnum == '2':
+          room.room_2()
+      elif roomnum == '3':
+          room.room_3()
+      elif roomnum == '4':
+          break
+      else:
+          print("<<<<<<잘못입력하셨습니다. 다시입력해주세요>>>>>>")
+          print("")
+ elif control == '3': # 3번 입력 시
   print("프로그램을 종료합니다.")
   break
  else:
