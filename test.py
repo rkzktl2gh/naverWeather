5f67bfae6b5965ca10fa6d2ac839e002904d2f8c
rkzktl2gh <48742418+rkzktl2gh@users.noreply.github.com>
2019-05-31 19:27:51 +0900

    코드 간결화-1

--- test.py
+++ test.py
@@ -1,25 +1,21 @@
 from naverWeather import *
 while(1):
  print("메뉴를 선택하세요.")
- print("1번 : 검색, 2번 : 종료")
+ print("1번 : 검색 | 2번 : 종료")
  control = input()
  if control == '1': # 1번 입력 시
   print("지역명을 입력해주세요.")
   area=input()
   if area in naverWeather.map_cityNum: # 올바른 도시명 입력 시
    print(naverWeather(area).getWeather())
-  elif area =='': print(naverWeather("서울").getWeather())
+  elif area =='': # 공백 입력 시
+      print(naverWeather("서울").getWeather())
   else: # 잘못된 도시명 입력 시
-   print("잘못된 도시명입니다. 다시 입력하세요")
-   area=input()
-   if area in naverWeather.map_cityNum:
-    print(naverWeather(area).getWeather())
-   else:
-      while(area not in naverWeather.map_cityNum):
-          print("잘못된 도시명입니다. 다시 입력하세요")
-          area=input()
-          if area in naverWeather.map_cityNum:
-              print(naverWeather(area).getWeather())
+   while(area not in naverWeather.map_cityNum):
+       print("잘못된 도시명입니다. 다시 입력하세요")
+       area=input()
+       if area in naverWeather.map_cityNum:
+           print(naverWeather(area).getWeather())
  elif control == '2': # 2번 입력 시
   print("프로그램을 종료합니다.")
   break
